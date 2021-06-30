27d0e6604f0e08459ddaeb8918a72f987ed0c9bb
Theresa Anyadike <nnennaanyadike@gmail.com>
2021-06-30 14:31:58 +0100

    changes

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ mongoose.connect(mongodbUrl, {
 }).catch(error => console.log(error.reason))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend')));
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
+        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
     });
 }
 
