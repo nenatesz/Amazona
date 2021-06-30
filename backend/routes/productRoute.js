b6f4ac2a6bdf8e420d0edf8495eb46ab160f1a6f
Theresa Anyadike <nnennaanyadike@gmail.com>
2021-06-30 19:52:23 +0100

    changes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -9,7 +9,7 @@ const {isAuth, isAdmin} = require('../util');
 const productRouter = express.Router();
 
 productRouter.get("/", expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
+    const products = await Product.find();
     // res.setHeader('People', 'Fun');
     // return res.status(200).send(products)
     res.send(products)
