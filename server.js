bec09ef37353ec7399164b2753925f7b6e1018b9
Theresa Anyadike <nnennaanyadike@gmail.com>
2021-06-30 14:07:34 +0100

    changes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'public', 'index.html'))
+        res.sendFile(path.join(__dirname, 'frontend', 'public/index.html'))
     });
 }
 
